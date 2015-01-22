6044ee6573d67735ee6ead67936206c2c9080269
Xon <tazipper1@hotmail.com>
2015-01-23 02:00:45 +0800

    Wireup permission check correctly

--- upload/library/SV/LazyImageLoader/XenForo/BbCode/Formatter/Base.php
+++ upload/library/SV/LazyImageLoader/XenForo/BbCode/Formatter/Base.php
@@ -9,8 +9,19 @@ class SV_LazyImageLoader_XenForo_BbCode_Formatter_Base  extends XFCP_SV_LazyImag
         parent::__construct();
         if (self::$lazy_imageTemplate === null)
         {
-            self::$lazy_imageTemplate = '<img src="'.SV_LazyImageLoader_Helper::_getLoaderIcon().'" data-src="%1$s" class="bbCodeImage%2$s" alt="[&#x200B;IMG]" data-url="%3$s" /><noscript><img src="%1$s" /></noscript>';
+            $url = SV_LazyImageLoader_Helper::_getLoaderIcon();
+            if ($url)
+            {
+                self::$lazy_imageTemplate = '<img src="'.$url.'" data-src="%1$s" class="bbCodeImage%2$s" alt="[&#x200B;IMG]" data-url="%3$s" /><noscript><img src="%1$s" /></noscript>';
+            }
+            else
+            {
+                self::$lazy_imageTemplate = false;
+            }
         }
-        $this->_imageTemplate = self::$lazy_imageTemplate;
+        if (self::$lazy_imageTemplate)
+        {
+            $this->_imageTemplate = self::$lazy_imageTemplate;
+        }
     }
 }
