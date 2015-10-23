8cbdc03aa92c87e60fe7fe9b318b332425715ba7
Xon <tazipper1@hotmail.com>
2015-10-23 08:42:05 +0800

    Support for Recent Attachments block

--- upload/library/SV/LazyImageLoader/Listener.php
+++ upload/library/SV/LazyImageLoader/Listener.php
@@ -8,4 +8,14 @@ class SV_LazyImageLoader_Listener
     {
         $extend[] = self::AddonNameSpace.$class;
     }
+
+    public static function template_create(&$templateName, array &$params, XenForo_Template_Abstract $template)
+    {
+        switch ($templateName)
+        {
+            case 'forum_list_recent_attachments': 
+                SV_LazyImageLoader_Helper::SetLazyLoadEnabled(true);
+                break;
+        }
+    }
 }
