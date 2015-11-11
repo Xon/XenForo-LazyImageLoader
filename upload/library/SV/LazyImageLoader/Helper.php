b4d46140913497569b41f9861914956c0c1b7dc4
Xon <tazipper1@hotmail.com>
2015-11-11 18:14:38 +0800

    Implement CSS intrinsic ratio scaling technique  for attachments

--- upload/library/SV/LazyImageLoader/Helper.php
+++ upload/library/SV/LazyImageLoader/Helper.php
@@ -2,6 +2,37 @@
 
 class SV_LazyImageLoader_Helper
 {
+    public static function getLazyLoadedImage($content, $params, XenForo_Template_Abstract $template)
+    {
+        $ratio = false;
+        if (SV_LazyImageLoader_Helper::$enable_lazyloading)
+        {
+            if (!empty($params['attachment']))
+            {
+                $attachment = $params['attachment'];
+                if (!empty($params['full']) && !empty($attachment['width']) && !empty($attachment['height']))
+                {
+                    $ratio = (100*$attachment['height']) / $attachment['width'];
+                }
+                else if (!empty($attachment['thumbnail_width']) && !empty($attachment['thumbnail_height']))
+                {
+                    $ratio = (100*$attachment['thumbnail_width']) / $attachment['thumbnail_height'];
+                }
+            }
+        }
+        $original = $params['1'] . $params['2'] . $params['3'] . $params['4'];
+
+        if ($ratio)
+        {
+            return '<div class="ratio-container" style="padding-bottom:'.$ratio.'%">' .
+                   $params['1'] . '"'. //'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" '.
+                   ' data-src="' . $params['2'] . $params['3'] . ' lazyload ' .$params['4'] . '</div>' .
+                   '<noscript>' . $original . '</noscript>';
+        }
+
+        return $original;
+    }
+
     public static function getLazySpinnerCss($content, $params, XenForo_Template_Abstract $template)
     {
         if (SV_LazyImageLoader_Helper::$enable_lazyloading)
